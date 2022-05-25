01af177e4b7520fa78d550bf1ed810c709a756fb
AndreMonica <andregodinhomonica@gmail.com>
2022-05-25 11:47:01 +0100

    IF/Else conditions for the remote control

--- Televisao/Comando.cs
+++ Televisao/Comando.cs
@@ -5,25 +5,55 @@ public class Comando : Televisao
 {
     public static void aumentarVolume()
     {
-        Volume++;
-        Televisao.consultar();
+        if (Volume < 100)
+        {
+            Volume++;
+            Televisao.consultar();
+        }
+        else
+        {
+            Console.WriteLine("Volume Maximo");
+        }
     }
 
     public static void diminuirVolume()
     {
-        Volume--;
-        Televisao.consultar();
+        if (Volume > 0)
+        {
+            Volume--;
+            Televisao.consultar();
+        }
+        else
+        {
+            Console.WriteLine("Vulume a 0");
+        }
     }
 
     public static void aumentarCanal()
     {
-        Canal++;
-        Televisao.consultar();
+        if (Canal < 999)
+        {
+            Canal++;
+            Televisao.consultar();
+        }
+        else
+        {
+            Canal = 1;
+            Televisao.consultar();
+        }
     }
 
     public static void diminuirCanal()
     {
-        Canal--;
-        Televisao.consultar();
+        if (Canal > 1 )
+        {
+            Canal--;
+            Televisao.consultar();
+        }
+        else
+        {
+            Canal = 999;
+            Televisao.consultar();
+        }
     }
 }
